1ff2eeafb28c2f7ee7010fc782b79390cf69b3e7
FirePhoenix21 <firedragon686@gmail.com>
2022-04-06 22:32:37 +1000

    Added depth change functionality
    Includes ability to change depth.  Note the player can shift into things and apparently cant move when on the ground but can on buildings???

--- Games Capstone Demo/Assets/Scripts/MovementScript.cs
+++ Games Capstone Demo/Assets/Scripts/MovementScript.cs
@@ -4,62 +4,91 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
-    public Rigidbody playerRigidbody;
-    public float yvelocity;
-    public float maxSpeed;
-    public float accel;
-    public float jumpaccel;
-    public float gravityModifier;
-    public bool canJump;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	//Variables
+	public Rigidbody playerRigidbody;
+	public float yvelocity;
+	public float maxSpeed;
+	public float accel;
+	public float jumpaccel;
+	public float gravityModifier;
+	public bool CanJump;
+	//Keys
+	public KeyCode jumpKey;
+	public KeyCode forwardKey;
+	public KeyCode backKey;
+	public KeyCode duckKey;
+	public KeyCode swapFKey;
+	public KeyCode swapBKey;
+	//Vals
+	public int minDepth;
+	public int maxDepth;
+	public int curDepth;
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    void FixedUpdate()
-    {
-        playerRigidbody.AddForce(Vector3.down * gravityModifier * GetComponent<Rigidbody>().mass);
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        //move left
-        if (Input.GetKey(KeyCode.A)&& playerRigidbody.velocity.x > maxSpeed *-1)
-        {
-            playerRigidbody.velocity += Vector3.left * accel;
-        }
-        //move right
-        if (Input.GetKey(KeyCode.D) && playerRigidbody.velocity.x < maxSpeed)
-        {
-            playerRigidbody.velocity += Vector3.right * accel;
-        }
-        //jump if y velocity 0 and spcbar
-        if (Input.GetKey(KeyCode.Space) && canJump)
-        {
-            playerRigidbody.velocity = Vector3.up * jumpaccel;
-            canJump = false;
-        }
+	void FixedUpdate()
+	{
+		playerRigidbody.AddForce(Vector3.down * gravityModifier * GetComponent<Rigidbody>().mass);
+	}
 
-        HardStop();
-        yvelocity = playerRigidbody.velocity.y;
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		//move left
+		if (Input.GetKey(backKey) && playerRigidbody.velocity.x > maxSpeed * -1)
+		{
+			playerRigidbody.velocity += Vector3.left * accel;
+		}
+		//move right
+		if (Input.GetKey(forwardKey) && playerRigidbody.velocity.x < maxSpeed)
+		{
+			playerRigidbody.velocity += Vector3.right * accel;
+		}
+		//jump if y velocity 0 and spcbar
+		if (Input.GetKey(jumpKey) && CanJump)
+		{
+			playerRigidbody.velocity = Vector3.up * jumpaccel;
+			CanJump = false;
+		}
+		//Change depth
+		if (Input.GetKeyDown(swapFKey))
+		{
+			ChangeDepth(-1);
+		}
+		if (Input.GetKeyDown(swapBKey))
+		{
+			ChangeDepth(1);
+		}
 
-    void HardStop()
-    {
-        // stop character if player isn't moving right or left
-        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
-        {
-            playerRigidbody.velocity = new Vector3(0,playerRigidbody.velocity.y,0);
-        }
-    }
+		// stop character if player isn't moving right or left
+		if (!Input.GetKey(backKey) && !Input.GetKey(forwardKey))
+		{
+			HardStop();
+		}
+		yvelocity = playerRigidbody.velocity.y;
+	}
 
-    void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.tag == "ground")
-        {
-            canJump = true;
-        }
-    }
+	void HardStop()
+	{
+		playerRigidbody.velocity = new Vector3(0, playerRigidbody.velocity.y, 0);
+	}
+
+	//Change z axis
+	void ChangeDepth(int newDepth)
+	{
+		curDepth = Mathf.Clamp(curDepth += newDepth, minDepth, maxDepth);
+		//gameObject.layer = curDepth + 7;
+		transform.position = new Vector3(transform.position.x, transform.position.y, curDepth * 5);
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "ground")
+		{
+			CanJump = true;
+		}
+	}
 }
