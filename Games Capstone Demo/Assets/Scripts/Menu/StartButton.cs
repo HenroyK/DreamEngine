626444d08ae24220c6030e8ab4e4c81711580a48
SWIN\102089690 <102089690@student.swin.edu.au>
2022-05-10 17:35:13 +1000

    Modified menu. Added cutscene after hitting start (currently progressed by hitting jump)

--- Games Capstone Demo/Assets/Scripts/Menu/StartButton.cs
+++ Games Capstone Demo/Assets/Scripts/Menu/StartButton.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour
 {
 	//Variables
 	public SceneAsset gameScene;
+	public List<Texture2D> introImages;
+	public GameObject cutsceneUI;
+	private int curScene = -1;
+
+	// Update is called once per frame
+	void Update()
+	{
+		//Progress when jump is pressed, loading when hitting the end of the images
+		if (Input.GetButtonDown("Jump") && curScene > 0)
+		{
+			curScene++;
+			if (curScene >= introImages.Count)
+			{
+				curScene = -1;
+				StartCoroutine(LoadAsyncScene());
+			}
+			else
+				cutsceneUI.GetComponent<RawImage>().texture = introImages[curScene];
+		}
+	}
 
 	//Button pressed
 	public void OnButtonPress()
 	{
-		StartCoroutine(LoadAsyncScene());
+		curScene = 1;
+		cutsceneUI.SetActive(true);
 	}
 
 	//Load scene (asynchronous)
