193131720f71ecaea5391a0f688db35a80a9775d
SWIN\102108294 <102108294@student.swin.edu.au>
2022-05-31 16:56:29 +1000

    Create the end level UI and the background music pauses when a UI is activated (pause, game over, end level)

--- Games Capstone Demo/Assets/Scripts/Game Controller Scripts/EndLevel.cs
+++ Games Capstone Demo/Assets/Scripts/Game Controller Scripts/EndLevel.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameOver : MonoBehaviour
+public class EndLevel : MonoBehaviour
 {
-    public GameObject gameOverMenuUI;
-    public Button retryBtn;
+    public GameObject endLevelMenuUI;
+    public Button reloadlevelBtn;
     public Button mainMenuBtn;
 
     private Pause pauseScript;
-    private bool playerDead = false;
 
-    // Set up variables / find scripts
+    // Start is called before the first frame update
     void Start()
     {
         GameObject gameController = GameObject.FindWithTag("GameController");
@@ -27,22 +26,18 @@ public class GameOver : MonoBehaviour
             Debug.Log("Error. Couldn't find Game Controller");
         }
 
-        Button btnRetry = retryBtn.GetComponent<Button>();
-        btnRetry.onClick.AddListener(RetryOnClick);
+        Button btnRetry = reloadlevelBtn.GetComponent<Button>();
+        btnRetry.onClick.AddListener(ReloadOnClick);
 
         Button btnMainMenu = mainMenuBtn.GetComponent<Button>();
         btnMainMenu.onClick.AddListener(MainMenuOnClick);
 
-        gameOverMenuUI.SetActive(false);
+        endLevelMenuUI.SetActive(false);
     }
 
-    // Reload on button press
-    void RetryOnClick()
+    void ReloadOnClick()
     {
-        if (playerDead == true)
-        {
-            Reload();
-        }
+        Reload();
     }
 
     void MainMenuOnClick()
@@ -50,18 +45,15 @@ public class GameOver : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-    // Reloads the current scene
     void Reload()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-    
-    // Turns on Game over UI
-    public void playerDied()
+
+    public void EndLevelReached()
     {
         pauseScript.disablePause();
-        playerDead = true;
-        gameOverMenuUI.SetActive(true);
+        endLevelMenuUI.SetActive(true);
         Time.timeScale = 0;
         gameObject.GetComponent<AudioSource>().Pause();
     }
