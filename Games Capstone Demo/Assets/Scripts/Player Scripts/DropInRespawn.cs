286543bf776eddc92332e3782a1d198e425a3080
SWIN\102108294 <102108294@student.swin.edu.au>
2022-08-16 17:55:53 +1000

    working on alternate respawn method

--- Games Capstone Demo/Assets/Scripts/Player Scripts/DropInRespawn.cs
+++ Games Capstone Demo/Assets/Scripts/Player Scripts/DropInRespawn.cs
@@ -8,13 +8,18 @@ public class DropInRespawn : MonoBehaviour
     public float spawnYAxis = 30;
 
     public Vector2 respawnPostionA = new Vector2(0, 30);
+    public Vector2 respawnPostionB = new Vector2(10, 30);
+    public Vector2 respawnPostionC = new Vector2(20, 30);
 
     private GameObject playerCharacter;
     private DepthBehaviour depthScript;
     private bool[] layerActive;
     private float[] layerZAxis;
     private int curLayer;
-    
+
+    private bool blockDetect = false;
+    private RaycastHit blockRaycastHit;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,8 +39,83 @@ public class DropInRespawn : MonoBehaviour
         curLayer = depthScript.curDepth;
     }
 
+    // function needs further work
+    // Advanced respawn function
+    // 1. checks current layer to see if any spawn positions are free
+    // 2. checks the the forward most layers relative to the current layer
+    // 3. brute force search all layers to find an open position
+    // 4. default respawn position
+    public void AltRespawnPlayer()
+    {
+        // currently only respawns the player on the layer they are currently on
+        curLayer = depthScript.curDepth;
+
+        // needs to be an array
+        Vector3 spawnPoint1 = new Vector3(
+                respawnPostionA.x, respawnPostionA.y, layerZAxis[curLayer]);
+        Vector3 spawnPoint2 = new Vector3(
+                respawnPostionB.x, respawnPostionB.y, layerZAxis[curLayer]);
+        Vector3 spawnPoint3 = new Vector3(
+                respawnPostionC.x, respawnPostionC.y, layerZAxis[curLayer]);
+
+        LayerMask mask = LayerMask.GetMask(new string[] { "GroundFloor", "Building" });
+        Quaternion weirdQuat = new Quaternion();
+        weirdQuat.eulerAngles = new Vector3(0, 0, 0);
+        if (!Physics.CheckBox(spawnPoint1, transform.localScale, weirdQuat, mask))
+        {
+            playerCharacter.transform.position = spawnPoint1;
+        }
+        else if (!Physics.CheckBox(spawnPoint2, transform.localScale, weirdQuat, mask))
+        {
+            playerCharacter.transform.position = spawnPoint2;
+        }
+        else if (!Physics.CheckBox(spawnPoint3, transform.localScale, weirdQuat, mask))
+        {
+            playerCharacter.transform.position = spawnPoint3;
+        }
+        else if (curLayer != 0)
+        {
+            int nextLayer = curLayer;
+
+            while (nextLayer >= 0)
+            {
+                nextLayer -= 1;
+
+                if (layerActive[nextLayer])
+                {
+                    spawnPoint1 = new Vector3(
+                    respawnPostionA.x, respawnPostionA.y, layerZAxis[nextLayer]);
+                    spawnPoint2 = new Vector3(
+                        respawnPostionB.x, respawnPostionB.y, layerZAxis[nextLayer]);
+                    spawnPoint3 = new Vector3(
+                        respawnPostionC.x, respawnPostionC.y, layerZAxis[nextLayer]);
+
+                    if (!Physics.CheckBox(spawnPoint1, transform.localScale, weirdQuat, mask))
+                    {
+                        playerCharacter.transform.position = spawnPoint1;
+                        break;
+                    }
+                    else if (!Physics.CheckBox(spawnPoint2, transform.localScale, weirdQuat, mask))
+                    {
+                        playerCharacter.transform.position = spawnPoint2;
+                        break;
+                    }
+                    else if (!Physics.CheckBox(spawnPoint3, transform.localScale, weirdQuat, mask))
+                    {
+                        playerCharacter.transform.position = spawnPoint3;
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
+
     public void RespawnPlayer()
     {
+        // currently only respawns the player on the layer they are currently on
+        curLayer = depthScript.curDepth;
+
         // finds the closest active layer, assumes that the
         // first active layer in the list is the closest
         int index = 0;
@@ -50,9 +130,6 @@ public class DropInRespawn : MonoBehaviour
                 index++;
             }
         }
-
-        // currently only respawns the player on the layer they are currently on
-        curLayer = depthScript.curDepth;
 
         if (layerActive[index])
         {
