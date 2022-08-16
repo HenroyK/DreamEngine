cc0b86d7b679078a3bc895c290dda650c119f678
SWIN\102102115 <102102115@student.swin.edu.au>
2022-08-16 17:29:00 +1000

    start of tutorial implementation

--- Games Capstone Demo/Assets/Scripts/World Object Scripts/UnpauseTrigger.cs
+++ Games Capstone Demo/Assets/Scripts/World Object Scripts/UnpauseTrigger.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnPauseTrigger : MonoBehaviour
+public class UnpauseTrigger : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         GameObject.FindGameObjectWithTag("GameController").GetComponent<GameContollerScript>().UnPauseTimer();
+        BroadcastMessage("DeleteTrigger");
+        
     }
 }
