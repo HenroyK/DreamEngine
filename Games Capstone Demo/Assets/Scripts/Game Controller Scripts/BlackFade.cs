7f774f4df882e58060142852c724290207da74da
SWIN\102089690 <102089690@student.swin.edu.au>
2022-08-23 18:00:51 +1000

    Saving work

--- Games Capstone Demo/Assets/Scripts/Game Controller Scripts/BlackFade.cs
+++ Games Capstone Demo/Assets/Scripts/Game Controller Scripts/BlackFade.cs
@@ -12,9 +12,32 @@ public class BlackFade : MonoBehaviour
 
 	private GameObject closest;
 	private float closeDist = 25;
+	private GameObject player;
+	private RaycastHit hit;
+
+	public void Start()
+	{
+		player = GameObject.FindWithTag("Player");
+	}
+
+	public void Update()
+	{
+
+		Physics.Raycast(player.transform.position, (player.transform.position - closest.transform.position).normalized,
+			out hit, 25, 1);
+		if (hit.collider.gameObject == closest)
+		{
+
+		}
+		else
+		{
+			closest = null;
+			closeDist = 25;
+		}
+	}
 
 	//Set the fade 0-100% of maxFade
-	public void SetFade(GameObject obj,float dist)
+	public void SetFade(GameObject obj, float dist)
 	{
 		//Listen to only the closest object for the fade
 		if (closest != obj && dist <= 25 && dist < closeDist)
@@ -27,9 +50,6 @@ public class BlackFade : MonoBehaviour
 			float percent = Mathf.InverseLerp(
 				0, 255, (((((dist / 25) - 1) * -1) * 100) / 100) * maxFade); //Quick Maff
 			blackFader.color = new Color(0, 0, 0, percent);
-
-			print(closest);
-			print(closeDist);
 		}
 	}
 
@@ -38,5 +58,11 @@ public class BlackFade : MonoBehaviour
 	{
 		blackFader.color = new Color(0, 0, 0, 0);
 	}
-	
+
+	public void FadeAmount(float amount)
+	{
+		float percent = Mathf.InverseLerp(
+			0, 255, (((((amount / 25) - 1) * -1) * 100) / 100) * maxFade); //Quick Maff
+		blackFader.color = new Color(0, 0, 0, percent);
+	}
 }
