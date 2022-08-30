32f48ef762b6c39f56d5887bce5b81ff2a865871
FirePhoenix21 <firedragon686@gmail.com>
2022-08-30 10:10:29 +1000

    Gizmos / fixes
    PathMove:
    Now checks for RigidBody position
    Draws a line of the path and spheres showing minimum distance
    Zipline:
    Now updates end position

--- Games Capstone Demo/Assets/Scripts/World Object Scripts/Interactables/PathMove.cs
+++ Games Capstone Demo/Assets/Scripts/World Object Scripts/Interactables/PathMove.cs
@@ -33,15 +33,15 @@ public class PathMove : MonoBehaviour
 		{
 			float step = Time.deltaTime * speed;
 			Vector3 vel = rb.velocity;
-
+			print(Vector3.Distance(rb.position, movePoints[pointIndex].GetComponent<Rigidbody>().position));
 			//Move towards point, going to next once close enough
 			if (!waiting)
 			{
-				Vector3 dir = movePoints[pointIndex].transform.position - rb.position;
+				Vector3 dir = movePoints[pointIndex].GetComponent<Rigidbody>().position - rb.position;
 				dir /= Time.fixedDeltaTime;
 				dir = Vector3.ClampMagnitude(dir, speed);
 				rb.velocity = dir;
-				if (Vector3.Distance(transform.position, movePoints[pointIndex].transform.position) < minDistance)
+				if (Vector3.Distance(rb.position, movePoints[pointIndex].GetComponent<Rigidbody>().position) < minDistance)
 				{
 					waiting = true;
 					if (pointIndex < movePoints.Length-1)
@@ -61,5 +61,21 @@ public class PathMove : MonoBehaviour
 			}
 		}
 
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+
+		//Draw the radius of being close enough and the path being taken
+		for (int i = 0; i < movePoints.Length; i++)
+		{
+			Vector3 pos = new Vector3(movePoints[i].transform.position.x+3, movePoints[i].transform.position.y, movePoints[i].transform.position.z);
+			Gizmos.DrawSphere(pos, minDistance);
+			if(i < movePoints.Length - 1)
+				Gizmos.DrawLine(pos, new Vector3(movePoints[i+1].transform.position.x + 3, movePoints[i+1].transform.position.y, movePoints[i+1].transform.position.z));
+			else
+				Gizmos.DrawLine(pos, new Vector3(movePoints[0].transform.position.x + 3, movePoints[0].transform.position.y, movePoints[0].transform.position.z));
+		}
 	}
 }
--- Games Capstone Demo/Assets/Scripts/World Object Scripts/Interactables/ZiplinePoint.cs
+++ Games Capstone Demo/Assets/Scripts/World Object Scripts/Interactables/ZiplinePoint.cs
@@ -6,7 +6,6 @@ public class ZiplinePoint : MonoBehaviour
 {
 	//Vars
 	public GameObject endPoint;
-	public bool isEnd;
 
 	private RaycastHit hitInfo;
 	private Vector3 endPos;
@@ -18,21 +17,18 @@ public class ZiplinePoint : MonoBehaviour
 
 	// Update is called once per frame
 	void Update()
-    {
-		if (!isEnd && Physics.Linecast(transform.position, endPoint.transform.position, out hitInfo))
+	{
+		endPos = new Vector3(endPoint.transform.position.x, endPoint.transform.position.y - 1, endPoint.transform.position.z);
+		if (Physics.Linecast(transform.position, endPoint.transform.position, out hitInfo))
 		{
-			if(hitInfo.collider.tag == "Player")
-				GameObject.FindWithTag("Player").GetComponent<MovementScript>().ZiplineTo(hitInfo.point,endPos,(hitInfo.point.y-endPos.y)/10);
+			if (hitInfo.collider.tag == "Player")
+				GameObject.FindWithTag("Player").GetComponent<MovementScript>().ZiplineTo(hitInfo.point, endPos, (hitInfo.point.y - endPos.y) / 10);
 		}
 	}
 
 	private void OnDrawGizmos()
 	{
-		Gizmos.color = Color.yellow;
-
-		if (!isEnd)
-		{
-			Gizmos.DrawLine(transform.position, endPoint.transform.position);
-		}
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine(transform.position, endPoint.transform.position);
 	}
 }
