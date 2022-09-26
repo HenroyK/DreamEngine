24ee3c5ce2ce972e911e0f71a8d1b8e83da01a5d
SWIN\102108294 <102108294@student.swin.edu.au>
2022-09-26 14:28:16 +1000

    started setup and testing for fade scene transitioning

--- Games Capstone Demo/Assets/Scripts/Menu/EndLevel.cs
+++ Games Capstone Demo/Assets/Scripts/Menu/EndLevel.cs
@@ -12,6 +12,7 @@ public class EndLevel : MonoBehaviour
     public Button reloadLevelBtn;
     public Button mainMenuBtn;
     public GameObject btnHighlight;
+    public GameObject blackOutSquare;
 
     private GameContollerScript gameControllerScript;
     private Pause pauseScript;
@@ -107,6 +108,35 @@ public class EndLevel : MonoBehaviour
                 }
             }
         }
+    }
+
+    // still testing this function
+    public IEnumerator FadeBlackOut(bool fadeToBlack = true, int fadeSpeed = 5)
+    {
+        Color objectColor = blackOutSquare.GetComponent<Image>().color;
+        float fadeAmount;
+
+        if (fadeToBlack)
+        {
+            while (blackOutSquare.GetComponent<Image>().color.a < 1)
+            {
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+        }
+        else
+        {
+            fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            blackOutSquare.GetComponent<Image>().color = objectColor;
+            yield return null;
+        }
+        
+        yield return new WaitForEndOfFrame();
     }
 
     void SwapSelected(int option)
