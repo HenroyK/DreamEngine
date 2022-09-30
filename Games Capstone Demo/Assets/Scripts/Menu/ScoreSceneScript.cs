77e981e0cb661b8c722343629164eb771eaf2011
Henry Kavadias-Barnes <102108294@student.swin.edu.au>
2022-09-30 21:02:15 +1000

    added a score scene script with basic scene transtion functionality, can be applied to the leaderboard scene

---
+++ Games Capstone Demo/Assets/Scripts/Menu/ScoreSceneScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScoreSceneScript : MonoBehaviour
+{
+    public int nextSceneNum = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Progress when jump is pressed, loading when hitting the end of the images
+        if (Input.GetButtonDown("Jump") || Input.GetButton("Enter"))
+        {
+            if (nextSceneNum != -1)
+            {
+                StartCoroutine(LoadAsyncScene());
+            }
+        }
+    }
+
+    //Load scene (asynchronous)
+    IEnumerator LoadAsyncScene()
+    {
+
+        AsyncOperation asyncLoad =
+            SceneManager.LoadSceneAsync(nextSceneNum);
+
+        //Wait until scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
