67ad00a29f2233178125735241bb7a8ad1263091
SWIN\102108294 <102108294@student.swin.edu.au>
2022-10-03 14:29:23 +1100

    needs fixing

--- Games Capstone Demo/Assets/Scripts/Menu/ScoreSceneScript.cs
+++ Games Capstone Demo/Assets/Scripts/Menu/ScoreSceneScript.cs
@@ -7,12 +7,23 @@ using UnityEngine.UI;
 
 public class ScoreSceneScript : MonoBehaviour
 {
+    public Image transitionFader;
+    public GameObject loadingNum;
+    public GameObject loadingText;
+
+    private float fadeTimer = 0;
+    private bool loaded = false;
+
     public int nextSceneNum = -1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (loaded)
+        {
+            fadeTimer += Time.deltaTime;
+            transitionFader.color = new Color(0, 0, 0, fadeTimer);
+        }
     }
 
     // Update is called once per frame
@@ -36,14 +47,41 @@ public class ScoreSceneScript : MonoBehaviour
     //Load scene (asynchronous)
     IEnumerator LoadAsyncScene()
     {
-
+        loadingText.gameObject.SetActive(true);
         AsyncOperation asyncLoad =
             SceneManager.LoadSceneAsync(nextSceneNum);
+        asyncLoad.allowSceneActivation = false;
 
         //Wait until scene fully loads
         while (!asyncLoad.isDone)
         {
+            //Whatever happened to just getting Text.text? seriously this is dumb
+            loadingNum.GetComponent<TextMeshProUGUI>().GetComponent<TMP_Text>().text = Mathf.Round((asyncLoad.progress * 100)) + "%";
+            loadingNum.gameObject.transform.Find("LoadingNum").GetComponent<TextMeshProUGUI>().GetComponent<TMP_Text>().text =
+                loadingNum.GetComponent<TextMeshProUGUI>().GetComponent<TMP_Text>().text;
+            if (asyncLoad.progress >= 0.9f)
+            {
+                loadingNum.gameObject.SetActive(false);
+                loadingText.GetComponent<TextMeshProUGUI>().GetComponent<TMP_Text>().text = "Done!";
+                loadingText.gameObject.transform.Find("LoadingText").GetComponent<TextMeshProUGUI>().GetComponent<TMP_Text>().text = "Done!";
+                if (asyncLoad.allowSceneActivation == false)
+                {
+                    loaded = true;
+                }
+            }
+            //Allow loading when faded
+            if (transitionFader.color.a >= 1)
+                asyncLoad.allowSceneActivation = true;
             yield return null;
         }
+
+        //AsyncOperation asyncLoad =
+        //    SceneManager.LoadSceneAsync(nextSceneNum);
+
+        ////Wait until scene fully loads
+        //while (!asyncLoad.isDone)
+        //{
+        //    yield return null;
+        //}
     }
 }
