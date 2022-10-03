f145cf014bf8f4469b116276f41035ff649a6334
SWIN\102094254 <102094254@student.swin.edu.au>
2022-10-03 13:45:32 +1100

    Total score screen now has a stamp

--- Games Capstone Demo/Assets/Scripts/ScoreScreenScripts/TotalScoreScript.cs
+++ Games Capstone Demo/Assets/Scripts/ScoreScreenScripts/TotalScoreScript.cs
@@ -9,10 +9,33 @@ public class TotalScoreScript : MonoBehaviour
     public Text level1Score;
     public Text level2Score;
     public Text totalScore;
+    public GameObject stamp;
+    public const int totalScoreS = 1250;
+    public const int totalScoreA = 1150;
+    public const int totalScoreB = 650;
+
+
     private void Start()
     {
         level1Score.text = "Level 1: " + PlayerStats.level1Score;
         level2Score.text = "Level 2: " + PlayerStats.level2Score;
         totalScore.text = "Total Score: " + PlayerStats.totalScore;
+        switch (PlayerStats.totalScore)
+        {
+            case > totalScoreS:
+                stamp.GetComponent<ScoreStampScript>().DisplayStamp(ScoreStampScript.Stamp.S);
+                break;
+            case > totalScoreA:
+                stamp.GetComponent<ScoreStampScript>().DisplayStamp(ScoreStampScript.Stamp.A);
+                break;
+            case > totalScoreB:
+                stamp.GetComponent<ScoreStampScript>().DisplayStamp(ScoreStampScript.Stamp.B);
+                break;
+            default:
+                stamp.GetComponent<ScoreStampScript>().DisplayStamp(ScoreStampScript.Stamp.C);
+                break;
+        }
+
+        
     }
 }
